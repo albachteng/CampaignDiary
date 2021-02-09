0377bfe7996197d56567f7bbf98a4f9b49cda6dd
Graham Albachten <albachteng@gmail.com>
2021-02-09 16:59:01 -0600

    fix npc selection script to allow filtering

--- _site/resources/scripts/script.js
+++ _site/resources/scripts/script.js
@@ -1,12 +1,20 @@
 let options = document.getElementsByTagName("input");
-let selection = document.getElementsByClassName("another");
-
-console.log(options); 
+let selection = document.getElementsByClassName("selection");
+let npcs = document.getElementsByClassName("npc");
 
 document.addEventListener('click', () => {
     for (let i = 0; i < options.length; i++) {
         if (options[i].checked) {
             selection[0].innerHTML = options[i].value;
+            for (let  j = 0; j < npcs.length; j++) {
+                npcs[j].classList.remove('hide'); // reset on click
+                if (!npcs[j].classList.contains(`${options[i].value.replace(/ /g, "")}`)) {
+                    npcs[j].classList.add('hide'); 
+                }
+            }
         }
     }
 }); 
+
+// NTS - rather than rely on Jekyll to handle the logic, let's write a script that 
+// hides the NPCs that are not selected (ie the ones whose place doesn't match)
