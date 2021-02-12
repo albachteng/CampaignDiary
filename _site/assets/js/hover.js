84595e487892cc183ece6683ac52b858bcf850b9
Graham Albachten <albachteng@gmail.com>
2021-02-12 13:58:27 -0600

    adjust hover.js to only add event listener if initial window is greater than 600px

--- _site/assets/js/hover.js
+++ _site/assets/js/hover.js
@@ -3,13 +3,18 @@ const main = document.getElementById("main");
 console.log(main)
 console.log(navbar); 
 
-navbar.addEventListener("mouseover", navbarIn, false);
-navbar.addEventListener("mouseleave", navbarOut, false); 
-
 function navbarIn() {
     main.setAttribute("style", "margin-left: 15rem;");
 }
 
 function navbarOut() {
     main.removeAttribute("style");
+}
+
+if (window.innerWidth <= 601) {
+    navbar.removeEventListener("mouseover", navbarIn);
+    navbar.removeEventListener("mouseleave", navbarOut);
+} else {
+    navbar.addEventListener("mouseover", navbarIn, false); 
+    navbar.addEventListener("mouseleave", navbarOut, false); 
 }
